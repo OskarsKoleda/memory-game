9cea367bbe3f7532f452a7c6d9ce0e31f6437d08
Oskars <oscar.koleda@gmail.com>
2025-04-13 23:50:06 +0300

    project created

---
+++ src/global.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+declare module "*.css";
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
